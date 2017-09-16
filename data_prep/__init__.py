f3f2da7cf9e747f78d91b8a815010db4e53d107f
Samuel Ling <sling@truman.edu>
2017-09-15 22:21:19 -0700

    added kaggle dir

--- data_prep/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 14 07:39:31 2017
-
-@author: sling
-"""
-
--- utilities/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 13 17:28:22 2017
-
-@author: sling
-"""
-
